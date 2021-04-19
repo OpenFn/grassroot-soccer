6e1ef563c09d9efa497f89136edb407bec6484db
mamadou <mamadou.lakhassane@gmail.com>
2021-04-19 14:24:54 +0000

    update relationships, #1

--- jobs/2.a.upsertInterventions.js
+++ jobs/2.a.upsertInterventions.js
@@ -4,28 +4,45 @@ upsert(
   fields(
     field('Name', dataValue('form.@name')),
     relationship('RecordType', 'Name', 'Intervention'),
-    field('Grant__c.CommCare_Ext_ID__c', dataValue('form.grant')),
-    field(
+    relationship(
+      'Grant_c',
+      'Grant__c.CommCare_Ext_ID__c',
+      dataValue('form.grant')
+    ),
+    relationship(
+      'Business_Unit_c',
       'Business_Unit__c.CommCare_Ext_ID__c',
       dataValue('form.business_unit')
     ),
-    field('Site__c.CommCare_Ext_ID__c', dataValue('form.site')),
-    field('Country__c.CommCare_Ext_ID__c', dataValue('form.site_country')),
-    field('Venue__c.CommCare_Ext_ID__c', dataValue('form.Venue')),
-    field('Venue_Type__c', dataValue('form.venue_type')),
-    field(
+    relationship(
+      'Site_c',
+      'Site__c.CommCare_Ext_ID__c',
+      dataValue('form.site')
+    ),
+    relationship(
+      'Country_c',
+      'Country__c.CommCare_Ext_ID__c',
+      dataValue('form.site_country')
+    ),
+    relationship(
+      'Venue_c',
+      'Venue__c.CommCare_Ext_ID__c',
+      dataValue('form.Venue')
+    ),
+    relationship(
+      'Curriculum__c',
       'Curriculum__c.CommCare_Ext_ID__c',
       dataValue('form.curriculum_selection.curriculum')
     ),
-    field(
-      'Graduation_Minimum__c',
-      dataValue('form.curriculum_selection.graduate_number')
-    ),
     field('Delivery_Method__c', dataValue('form.delivery_method')),
     field('Class_Group_Team__c', dataValue('form.class_grade')),
-    field('Geographical_area__c', dataValue('form.geographic_area')),
+    relationship(
+      'Venue_c',
+      'Geographical_area__c',
+      dataValue('form.geographic_area')
+    ),
     field('Pre_Post_Completed__c', dataValue('form.prepost_administered')),
-    field('Coach_A__c.CommCare_Ext_ID__c', dataValue('form.coaches')),
+    relationship('Person_c', 'Coach_A__c.CommCare_Ext_ID__c', dataValue('form.coaches')),
     field('Start_Date__c', dataValue('form.intervention_dates.start_date')),
     field('End_Date__c', dataValue('form.intervention_dates.end_date'))
   )
