cd4cacb025867cd56be33cf3b26131f649dc2006
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-03 12:37:56 -0400

    aissatou@openfn.org's change on OpenFn.org

--- jobs/2.c.upsertInterventionNotes.js
+++ jobs/2.c.upsertInterventionNotes.js
@@ -3,7 +3,7 @@ upsert(
   'Event__c',
   'Name',
   fields(
-    field('Name', dataValue('form.intervention_name')),//TODO: Aicha to update intervention_name path
+    field('Name', dataValue('form.intervention_name')),
     field('Notes__c', dataValue('form.intervention_notes_to_save')),
     relationship('RecordType', 'Name', 'Intervention')
   )
