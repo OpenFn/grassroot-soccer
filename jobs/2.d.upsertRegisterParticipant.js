306d674fb5444630bcc3528c200bf177e891cad7
aleksa-krolls <aleksagk@gmail.com>
2021-05-04 19:08:40 +0300

    upsert attendance as a separate operation

--- jobs/2.d.upsertRegisterParticipant.js
+++ jobs/2.d.upsertRegisterParticipant.js
@@ -1,52 +1,38 @@
-upsert(
-  'Person__c',
-  'Participant_Identification_Number_PID__c',
-  fields(
-    field('Notes__c', dataValue('form.intervention_notes_to_save')),
-    relationship('RecordType', 'Name', 'Intervention'),
-    field('Participant_Identification_Number_PID__c', dataValue('form.case.@case_id')),
-
-    relationship(
-      'Attendance__c',
-      'First_Name__c',
-      dataValue('form.question1.participant_first_name')
-    ),
-    relationship(
-      'Attendance__c',
-      'Surname__c',
-      dataValue('form.question1.participant_surname')
-    ),
-    relationship(
-      'Attendance__c',
-      'Name',
-      dataValue('form.question1.participant_full_name')
-    ),
-    relationship(
-      'Attendance__c',
-      'Date_of_Birth__c',
-      dataValue('form.question1.date_of_birth')
-    ),
-    relationship(
-      'Attendance__c',
-      'Age__c',
-      dataValue('form.question1.age_in_years')
-    ),
-    relationship(
-      'Attendance__c',
-      'Sex_c',
-      dataValue('form.question1.gender')
-    ),
-    relationship(
-      'Attendance__c',
-      'Mobile_Number_1__c',
-      dataValue('form.question1.mobile_number')
-    ),
-    
-    relationship(
-      'Attendance__c',
-      '"School_name_person__c	"',
-      dataValue('form.question1.school_name')
-    ),
-
-  )
+combine(
+  //First we insert Person record
+  upsert(
+    'Person__c',
+    'Participant_Identification_Number_PID__c',
+    fields(
+      field('Notes__c', dataValue('form.intervention_notes_to_save')),
+      relationship('RecordType', 'Name', 'Intervention'),
+      field('Participant_Identification_Number_PID__c', dataValue('form.case.@case_id')),
+    )
+  ),
+  //Then we upsert related Attendance records
+  upsert(
+    'Attendance__c',
+    'CommCare_Ext_ID__c',
+    fields(
+      field('CommCare_Ext_ID__c', state => {
+        var eventid = dataValue('form.intervention_name')(state);
+        var personid = dataValue('form.case.@case_id')(state);
+        return personid + '-' + eventid;
+      }),
+      relationship( //Attendance looks up to Persn via the case_id
+        'Person__c',
+        'Participant_Identification_Number_PID__c',
+        dataValue('form.case.@case_id')
+      ),
+      relationship( //Attendance looks up to Event via the intervention_name
+        'Event__c',
+        'Name',
+        dataValue('form.intervention_name')
+      ),
+      field('Date_of_Birth__c', dataValue('form.question1.date_of_birth')),
+      field('Sex_c', dataValue('form.question1.gender')),
+      field('Age__c', dataValue('form.question1.age_in_years')),
+      field('Mobile_Number_1__c', dataValue('form.question1.mobile_number')),
+      field('School_name_person__c', dataValue('form.question1.school_name')),
+    ))
 );
