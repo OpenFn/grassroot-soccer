d0a5ec4d485ae96116dbb2ac42ac3a7d1014344c
Aleksa Krolls <aleksagk@gmail.com>
2021-05-04 19:41:07 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/2.d.upsertRegisterParticipant.js
+++ jobs/2.d.upsertRegisterParticipant.js
@@ -5,7 +5,7 @@ alterState(state => {
     'Participant_Identification_Number_PID__c',
     fields(
       field('Notes__c', dataValue('form.intervention_notes_to_save')),
-      relationship('RecordType', 'Name', 'Intervention'),
+      relationship('RecordType', 'Name', 'Participant'),
       field('Participant_Identification_Number_PID__c', dataValue('form.case.@case_id'))
     )
   )(state).then(state => {
