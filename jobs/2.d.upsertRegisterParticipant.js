da0a9d747b2c1c893d1ca343735179072d0af08b
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-04 06:17:11 -0400

    aissatou@openfn.org's change on OpenFn.org

--- jobs/2.d.upsertRegisterParticipant.js
+++ jobs/2.d.upsertRegisterParticipant.js
@@ -1,5 +1,5 @@
 upsert(
-  'Person_c',
+  'Person__c',
   'Participant_Identification_Number_PID__c',
   fields(
     field('Notes__c', dataValue('form.intervention_notes_to_save')),
@@ -7,17 +7,17 @@ upsert(
     
 
     relationship(
-      'Attendance_c',
+      'Attendance__c',
       'First_Name__c',
       dataValue('form.question1.participant_first_name')
     ),
     relationship(
-      'Attendance_c',
+      'Attendance__c',
       'Surname__c',
       dataValue('form.question1.participant_surname')
     ),
     relationship(
-      'Attendance_c',
+      'Attendance__c',
       'Name',
       dataValue('form.question1.participant_full_name')
     ),
@@ -27,23 +27,23 @@ upsert(
       dataValue('form.question1.date_of_birth')
     ),
     relationship(
-      'Attendance_c',
+      'Attendance__c',
       'Age__c',
       dataValue('form.question1.age_in_years')
     ),
     relationship(
-      'Attendance_c',
+      'Attendance__c',
       'Sex_c',
       dataValue('form.question1.gender')
     ),
     relationship(
-      'Attendance_c',
+      'Attendance__c',
       'Mobile_Number_1__c',
       dataValue('form.question1.mobile_number')
     ),
     
     relationship(
-      'Attendance_c',
+      'Attendance__c',
       '"School_name_person__c	"',
       dataValue('form.question1.school_name')
     ),
