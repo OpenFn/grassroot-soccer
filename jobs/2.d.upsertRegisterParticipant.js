ebd0868c4e98631aaced167724e16761477eafd0
Aleksa Krolls <aleksagk@gmail.com>
2021-05-05 09:40:28 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/2.d.upsertRegisterParticipant.js
+++ jobs/2.d.upsertRegisterParticipant.js
@@ -1,6 +1,4 @@
-//First we insert Person record
-alterState(state => {
-  return upsert(
+upsert(
     'Person__c',
     'Participant_Identification_Number_PID__c',
     fields(
@@ -14,12 +12,8 @@ alterState(state => {
       field('Mobile_Number_1__c', dataValue('form.question1.mobile_number')),
       field('School_name_person__c', dataValue('form.question1.school_name'))
     )
-  )(state)
-  }); 
-  
-  alterState(state => {
-    //Then we upsert related Attendance records
-    return upsert(
+)
+upsert(
       'Attendance__c',
       'CommCare_Ext_ID__c',
       fields(
@@ -42,5 +36,51 @@ alterState(state => {
         ),
         field('Date_of_Birth__c', dataValue('form.question1.date_of_birth')),
       )
-    )(state);
-  });
+  )
+
+// //First we insert Person record
+// alterState(state => {
+//   return upsert(
+//     'Person__c',
+//     'Participant_Identification_Number_PID__c',
+//     fields(
+//       field('Notes__c', dataValue('form.intervention_notes_to_save')),
+//       field('First_Name__c', dataValue('form.question1.participant_first_name')),
+//       field('Surname__c', dataValue('form.question1.participant_surname')),
+//       relationship('RecordType', 'Name', 'Participant'),
+//       field('Participant_Identification_Number_PID__c', dataValue('form.case.@case_id')),
+//       field('Sex__c', dataValue('form.question1.gender')),
+//       //field('Age__c', dataValue('form.question1.age_in_years')), //This is a SF formula field, cannot map
+//       field('Mobile_Number_1__c', dataValue('form.question1.mobile_number')),
+//       field('School_name_person__c', dataValue('form.question1.school_name'))
+//     )
+//   )(state)
+//   }); 
+  
+//   alterState(state => {
+//     //Then we upsert related Attendance records
+//     return upsert(
+//       'Attendance__c',
+//       'CommCare_Ext_ID__c',
+//       fields(
+//         field('CommCare_Ext_ID__c', state => {
+//           var eventid = dataValue('form.intervention_name')(state);
+//           var personid = dataValue('form.case.@case_id')(state);
+//           return personid + '-' + eventid;
+//         }),
+//         relationship(
+//           //Attendance looks up to Persn via the case_id
+//           'Person_Attendance__r',
+//           'Participant_Identification_Number_PID__c',
+//           dataValue('form.case.@case_id')
+//         ),
+//         relationship(
+//           //Attendance looks up to Event via the intervention_name
+//           'Event__r',
+//           'Name',
+//           dataValue('form.intervention_name')
+//         ),
+//         field('Date_of_Birth__c', dataValue('form.question1.date_of_birth')),
+//       )
+//     )(state);
+//   });
