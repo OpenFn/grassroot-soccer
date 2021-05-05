7931448fa593c485e2ba6c674e4e5c93b8f48677
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-05 11:13:42 -0400

    aissatou@openfn.org's change on OpenFn.org

--- jobs/1.getForms.js
+++ jobs/1.getForms.js
@@ -18,7 +18,7 @@ get(
       //xmlns: 'http://openrosa.org/formdesigner/304CABA9-C8A8-4360-83C6-5E2EC1D2C2AC', // Intervention Notes form
       //xmlns: 'http://openrosa.org/formdesigner/3B4B1C64-092A-4922-B033-94D80A11D960C', // Coach Session Register form
       //xmlns: 'http://openrosa.org/formdesigner/4CFAC371-88F7-4349-BEB2-3DA2BDE445EA', //Malaria Testing Event Snapshot
-      xmlns: 'http://openrosa.org/formdesigner/56DB98AC-67EC-4BBA-91FC-9443E84E43CE', // PHQ9
+      xmlns: 'http://openrosa.org/formdesigner/551BCED9-077B-40EE-849D-0416756B9A75', // PHQ9
 
       received_on_start: '2021-05-05',
       received_on_end: '2021-05-12'
