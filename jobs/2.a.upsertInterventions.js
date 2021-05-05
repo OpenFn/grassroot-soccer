c114389a78d59f47a679502314b118680e785f92
Aleksa Krolls <aleksagk@gmail.com>
2021-05-05 17:35:42 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/2.a.upsertInterventions.js
+++ jobs/2.a.upsertInterventions.js
@@ -49,7 +49,7 @@ upsert(
     //  dataValue('form.geographic_area')
     //),
     field('Pre_Post_Administered__c', dataValue('form.prepost_administered')),
-    relationship('Coach_A__r', 'CommCare_Ext_ID__c', state => {return state.coaches[0]}),
+    //relationship('Coach_A__r', 'CommCare_Ext_ID__c', state => {return state.coaches[0]}),
     relationship('Coach_B__r', 'CommCare_Ext_ID__c', state => {return state.coaches[1]}),
     //relationship('Coach_C__r', 'CommCare_Ext_ID__c', state => {return state.coaches[2]}),
    // relationship('Coach_D__r', 'CommCare_Ext_ID__c', state => {return state.coaches[3]}),
