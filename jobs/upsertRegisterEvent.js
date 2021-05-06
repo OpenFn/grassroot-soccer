089c647c94a9aae50dcbcc1d13b9a64cfcf07395
Berian <chaiwa.berian@gmail.com>
2021-05-06 19:41:36 +0200

    Update upsertRegisterEvent.js

--- jobs/upsertRegisterEvent.js
+++ jobs/upsertRegisterEvent.js
@@ -4,9 +4,9 @@ upsert(
   fields(
     field('Name', dataValue('form.event_information.Event_Name')),
     field('Business_Unit__c', dataValue('form.event_information.business_unit')),
-    field('Site__c', dataValue('form.event_information.site')),
-    field('Country__c', dataValue('form.event_information.site_country')),
-    field('Venue__c', dataValue('form.event_information.Venue')),
+    relationship('Site__r','CommCare_Ext_ID__c', dataValue('form.event_information.site')),
+    relationship('Venue__r', 'CommCare_Ext_ID__c',dataValue('form.event_information.Venue')),
+    field('Country__c', dataValue('form.event_information.site_country')),    
     field('Date__c', dataValue('form.event_information.event_date')),
     field('Event_Type__c', dataValue('form.event_information.event_type')),
     field('Coordinator__c', dataValue('form.event_information.event_coordinator')),
