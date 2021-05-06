14c6abe44792e62efaf6b0271988ebbd948277fc
Aleksa Krolls <aleksagk@gmail.com>
2021-05-06 19:17:31 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/upsertHomeVisitLog.js
+++ jobs/upsertHomeVisitLog.js
@@ -3,9 +3,10 @@ upsert(
   'CommCare_Ext_ID__c',
   fields(
     field('CommCare_Ext_ID__c', dataValue('id')),
-    field('Event__c', dataValue('form.basic_information.intervention_hidden')),
-    field('First_Name__c', dataValue('form.basic_information.participant_first_name')),
-    field('Surname__c', dataValue('form.basic_information.participant_surname')),
+    relationship('Event__r', 'CommCare_Ext_ID__c', dataValue('form.basic_information.intervention_hidden')),
+    relationship('Person_visiting__c', 'Participant_Identification_Number_PID__c', dataValue('form.case.@case_id')),
+    // field('First_Name__c', dataValue('form.basic_information.participant_first_name')),
+    // field('Surname__c', dataValue('form.basic_information.participant_surname')),
     field('Consent_Given__c', dataValue('form.basic_information.consent_received')),
     field('Sex_c', dataValue('form.basic_information.gender')),
     field('From_Venue__c', dataValue('form.basic_information.venue_hidden')),
