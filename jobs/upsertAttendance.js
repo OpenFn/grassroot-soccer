6f8dbff19953d070be73cf3aafff8d00e7b70177
Aleksa Krolls <aleksagk@gmail.com>
2021-05-06 18:52:32 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/upsertAttendance.js
+++ jobs/upsertAttendance.js
@@ -41,7 +41,7 @@ alterState(state => {
 upsert('Attendance__c', 'CommCare_Ext_ID__c', state => ({
   ...fields(...state.data.dynamicFields),
   ...fields(
-    relationship('Event__r', 'CommCare_Ext_ID__c', dataValue('form.attendance_list.intervention_name')),
+    relationship('Event__r', 'CommCare_Ext_ID__c', dataValue('form.intervention_name')),
     field('Name', dataValue('form.attendance_list.update_participant_cases.item.participant_id')),
     field('CommCare_Ext_ID__c', dataValue('commcare_external_id')),
     relationship('Person_Attendance__r', 'Participant_Identification_Number_PID__c', dataValue('form.attendance_list.update_participant_cases.item.participant_name')),
