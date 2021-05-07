596d864d10d76a175d120b9f7664f5f33c41370d
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-07 08:34:27 -0400

    chaiwa@openfn.org's change on OpenFn.org

--- jobs/2.a.upsertInterventions.js
+++ jobs/2.a.upsertInterventions.js
@@ -1,7 +1,8 @@
 alterState((state) => {
   
   function clean(str){
-    return str?.split('_').map(word=>{let new_word = word.toString().toLowerCase();return (new_word.slice(0,1).toUpperCase()+new_word.slice(1))})?.join(' ')
+    if(!!str)
+    return str.split('_').map(word=>{let new_word = word.toString().toLowerCase();return (new_word.slice(0,1).toUpperCase()+new_word.slice(1))}).join(' ')
 
   }
   const { data } = state;
