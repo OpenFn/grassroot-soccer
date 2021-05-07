f04069165a03d0d0c333b1bfedcd42b4c8da6f4d
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-07 08:33:13 -0400

    chaiwa@openfn.org's change on OpenFn.org

--- jobs/2.a.upsertInterventions.js
+++ jobs/2.a.upsertInterventions.js
@@ -8,7 +8,7 @@ alterState((state) => {
   const coach = state.data.form.coaches;
   state.coaches = coach ? coach.split(' ') : coach; 
   state.data.form.delivery_method = clean(state.data.form.delivery_method);
-  state.data.form.delivery_method = clean(state.data.form['step_3_-_the_big_5.faciliation']);
+  // state.data.form.delivery_method = clean(state.data.form['step_3_-_the_big_5.faciliation']);
   
 
   return state; 
