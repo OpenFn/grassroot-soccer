4ddb61c5127c08b7a66f7a6a636bc5fb4f5fd309
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-07 17:15:51 -0400

    chaiwa@openfn.org's change on OpenFn.org

--- jobs/2.a.upsertInterventions.js
+++ jobs/2.a.upsertInterventions.js
@@ -28,37 +28,38 @@ alterState((state) => {
   return state; 
 }); 
 
-// upsert(
-//   'Event__c',
-//   'CommCare_Ext_ID__c',
-//   fields(
-//     field('CommCare_Ext_ID__c', dataValue('form.name_of_intervention')),
-//     field('Grant_Text__c', dataValue('form.grant')),
-//     relationship('RecordType', 'Name', 'Intervention'),
-//     field('Business_Unit__c', state => {
-//       const bu = dataValue('form.business_unit')(state); 
-//       return bu==='X' ? 'GRS Zambia' : bu; 
-//     }),
-//     relationship(
-//       'Site__r',
-//       'CommCare_Ext_ID__c',
-//       dataValue('form.site')
-//     ),
-//     relationship(
-//       'Venue__r',
-//       'CommCare_Ext_ID__c',
-//       dataValue('form.Venue')
+upsert(
+  'Event__c',
+  'CommCare_Ext_ID__c',
+  fields(
+    ...state=>state.data.coaches,
+    field('CommCare_Ext_ID__c', dataValue('form.name_of_intervention')),
+    field('Grant_Text__c', dataValue('form.grant')),
+    relationship('RecordType', 'Name', 'Intervention'),
+    field('Business_Unit__c', state => {
+      const bu = dataValue('form.business_unit')(state); 
+      return bu==='X' ? 'GRS Zambia' : bu; 
+    }),
+    relationship(
+      'Site__r',
+      'CommCare_Ext_ID__c',
+      dataValue('form.site')
+    ),
+    relationship(
+      'Venue__r',
+      'CommCare_Ext_ID__c',
+      dataValue('form.Venue')
       
-//     ),
-//     relationship(
-//       'Curriculum__r',
-//       'CommCare_Ext_ID__c',
-//       dataValue('form.curriculum_selection.curriculum')
-//     ),
-//     field('Delivery_Method__c', dataValue('form.delivery_method')),
-//     field('Class_Group_Team__c', dataValue('form.class_grade')),
-//     field('Pre_Post_Administered__c', dataValue('form.prepost_administered')),
-//     field('Start_Date__c', dataValue('form.intervention_dates.start_date')),
-//     field('End_Date__c', dataValue('form.intervention_dates.end_date'))
-//   )
-// );
+    ),
+    relationship(
+      'Curriculum__r',
+      'CommCare_Ext_ID__c',
+      dataValue('form.curriculum_selection.curriculum')
+    ),
+    field('Delivery_Method__c', dataValue('form.delivery_method')),
+    field('Class_Group_Team__c', dataValue('form.class_grade')),
+    field('Pre_Post_Administered__c', dataValue('form.prepost_administered')),
+    field('Start_Date__c', dataValue('form.intervention_dates.start_date')),
+    field('End_Date__c', dataValue('form.intervention_dates.end_date'))
+  )
+);
