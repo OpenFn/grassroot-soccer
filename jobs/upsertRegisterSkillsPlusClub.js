52d079ce3187a3c46bd768469e94fd61dc647f2e
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-10 10:30:13 -0400

    aissatou@openfn.org's change on OpenFn.org

--- jobs/upsertRegisterSkillsPlusClub.js
+++ jobs/upsertRegisterSkillsPlusClub.js
@@ -4,7 +4,7 @@ upsert(
   'CommCare_Ext_ID__c',
   fields(
     field('CommCare_Ext_ID__c', dataValue('form.name_of_skillz_plus_club')),
-    relationship('RecordType', 'CommCare_Ext_ID__c', 'Intervention'),
+    relationship('RecordType', 'Name', 'Intervention'),
     relationship('Site__r', 'CommCare_Ext_ID__c', dataValue('form.skillz_plus_site')),
     relationship('Venue__r', 'CommCare_Ext_ID__c', dataValue('form.skillz_plus_venue')),
     relationship('Coach_A__r','CommCare_Ext_ID__c', dataValue('form.coaches'))
