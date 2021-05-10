8501587a0bb560d77c7e9d004d129bf84804977c
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-10 11:53:46 -0400

    aissatou@openfn.org's change on OpenFn.org

--- jobs/upsertServiceReferral.js
+++ jobs/upsertServiceReferral.js
@@ -3,11 +3,13 @@ upsert(
   'GRS_Referral_Agg__c',
   'CommCare_Ext_ID__c',
   fields(
-    field('CommCare_Ext_ID__c', dataValue('id')),
+    ('Enter_Partner_s_Name__c', dataValue('form.general_referral_information.partner_or_organization_referred_to'))
+    field('Type_of_Referral__c', dataValue('form.general_referral_information.type_of_referral')),
     field('Participants_Full_Name__c', dataValue('form.client_registration.participant_full_name')),
     field('Institution_Referred_To_3__c', dataValue('form.client_registration.participants_age_to_save')),
     field('Institution_Referred_To_3_Contact_Number__c', dataValue('form.client_registration.gender')),
     field('Parent_Guardian_Contact_Number__c', dataValue('form.client_registration.mobile_number')),
+    field('CommCare_Ext_ID__c', dataValue('id')),
     //field('Venue__c', dataValue('form.client_registration.school_name')),
     //field('Grade__c', dataValue('form.client_registration.grade')),
     //field('Class__c', dataValue('form.client_registration.class')),
