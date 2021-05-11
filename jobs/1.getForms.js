39a5c1a041417c2c89aed504bea571e0b5075936
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-11 05:58:51 -0400

    aissatou@openfn.org's change on OpenFn.org

--- jobs/1.getForms.js
+++ jobs/1.getForms.js
@@ -4,7 +4,7 @@ get(
     query: {
       limit: 50, //max limit: 1000
       offset: state.meta && state.meta.next ? state.meta.limit + state.meta.offset : 0,
-      received_on_start: '2021-05-10',
+      received_on_start: '2021-05-09',
       received_on_end: '2021-05-12',
     },
   },
