9f0bbb9bcbf5868c41537a8f3693eec781483696
Chaiwa Berian <chaiwa.berian@gmail.com>
2021-05-12 13:14:43 +0200

    Fix case_id

--- jobs/2.d.upsertRegisterParticipant.js
+++ jobs/2.d.upsertRegisterParticipant.js
@@ -11,10 +11,7 @@ alterState(state => {
 beta.each(
   merge(
     dataPath('form.question1[*]'),
-    fields(
-      field('intervention_notes_to_save', dataValue('form.intervention_notes_to_save')),
-      field('case_id', dataValue('form.case.@case_id'))
-    )
+    fields(field('intervention_notes_to_save', dataValue('form.intervention_notes_to_save')))
   ),
 
   upsert(
@@ -25,7 +22,7 @@ beta.each(
       field('First_Name__c', dataValue('participant_first_name')),
       field('Surname__c', dataValue('participant_surname')),
       relationship('RecordType', 'Name', 'Participant'),
-      field('Participant_Identification_Number_PID__c', dataValue('case_id')),
+      field('Participant_Identification_Number_PID__c', state => state.data.case['@case_id']),
       field('Sex__c', dataValue('gender')),
       //field('Age__c', dataValue('form.question1.age_in_years')), //This is a SF formula field, cannot map
       field('Mobile_Number_1__c', dataValue('mobile_number')),
@@ -35,27 +32,21 @@ beta.each(
 );
 
 each(
-  merge(
-    dataPath('form.question1[*]'),
-    fields(
-      field('intervention_name', dataValue('form.intervention_name')),
-      field('case_id', dataValue('form.case.@case_id'))
-    )
-  ),
+  merge(dataPath('form.question1[*]'), fields(field('intervention_name', dataValue('form.intervention_name')))),
   upsert(
     'Attendance__c',
     'CommCare_Ext_ID__c',
     fields(
       field('CommCare_Ext_ID__c', state => {
         var eventid = dataValue('intervention_name')(state);
-        var personid = dataValue('case_id')(state);
+        var personid = state.data.case['@case_id'];
         return personid + '-' + eventid;
       }),
       relationship(
         //Attendance looks up to Persn via the case_id
         'Person_Attendance__r',
         'Participant_Identification_Number_PID__c',
-        dataValue('case_id')
+        state => state.data.case['@case_id']
       ),
       relationship(
         //Attendance looks up to Event via the intervention_name
@@ -68,7 +59,7 @@ each(
   )
 );
 
-// //First we insert Person record
+//First we insert Person record
 // alterState(state => {
 //   return upsert(
 //     'Person__c',
@@ -78,39 +69,39 @@ each(
 //       field('First_Name__c', dataValue('form.question1.participant_first_name')),
 //       field('Surname__c', dataValue('form.question1.participant_surname')),
 //       relationship('RecordType', 'Name', 'Participant'),
-//       field('Participant_Identification_Number_PID__c', dataValue('form.case.@case_id')),
+//       field('Participant_Identification_Number_PID__c', state => state.data.case['@case_id']),
 //       field('Sex__c', dataValue('form.question1.gender')),
 //       //field('Age__c', dataValue('form.question1.age_in_years')), //This is a SF formula field, cannot map
 //       field('Mobile_Number_1__c', dataValue('form.question1.mobile_number')),
 //       field('School_name_person__c', dataValue('form.question1.school_name'))
 //     )
-//   )(state)
-//   });
+//   )(state);
+// });
 
-//   alterState(state => {
-//     //Then we upsert related Attendance records
-//     return upsert(
-//       'Attendance__c',
-//       'CommCare_Ext_ID__c',
-//       fields(
-//         field('CommCare_Ext_ID__c', state => {
-//           var eventid = dataValue('form.intervention_name')(state);
-//           var personid = dataValue('form.case.@case_id')(state);
-//           return personid + '-' + eventid;
-//         }),
-//         relationship(
-//           //Attendance looks up to Persn via the case_id
-//           'Person_Attendance__r',
-//           'Participant_Identification_Number_PID__c',
-//           dataValue('form.case.@case_id')
-//         ),
-//         relationship(
-//           //Attendance looks up to Event via the intervention_name
-//           'Event__r',
-//           'Name',
-//           dataValue('form.intervention_name')
-//         ),
-//         field('Date_of_Birth__c', dataValue('form.question1.date_of_birth')),
-//       )
-//     )(state);
-//   });
+// alterState(state => {
+//   //Then we upsert related Attendance records
+//   return upsert(
+//     'Attendance__c',
+//     'CommCare_Ext_ID__c',
+//     fields(
+//       field('CommCare_Ext_ID__c', state => {
+//         var eventid = dataValue('form.intervention_name')(state);
+//         var personid = dataValue('form.case.@case_id')(state);
+//         return personid + '-' + eventid;
+//       }),
+//       relationship(
+//         //Attendance looks up to Persn via the case_id
+//         'Person_Attendance__r',
+//         'Participant_Identification_Number_PID__c',
+//         dataValue('form.case.@case_id')
+//       ),
+//       relationship(
+//         //Attendance looks up to Event via the intervention_name
+//         'Event__r',
+//         'Name',
+//         dataValue('form.intervention_name')
+//       ),
+//       field('Date_of_Birth__c', dataValue('form.question1.date_of_birth'))
+//     )
+//   )(state);
+// });
