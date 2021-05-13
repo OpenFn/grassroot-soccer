ba4237dc8541d6ed4b9e311523393b07585fd90f
Chaiwa Berian <chaiwa.berian@gmail.com>
2021-05-13 14:20:40 +0200

    Stub solution for #11

--- jobs/upsertAttendance.js
+++ jobs/upsertAttendance.js
@@ -1,58 +1,60 @@
 alterState(state => {
-  const present = dataValue('form.attendance_list.update_participant_cases.item.attendance_session')(state).toLowerCase();
-
-  function getSessionValue() {
-    let value;
-    switch (present) {
+  function getSessionValue(present) {
+    switch (present.toString().toLowerCase()) {
       case 'yes':
-        value = 'X';
-        break;
+        return 'X';
       case 'no':
-        value = 'A';
-        break;
+        return 'A';
       case '':
-        value = 'N';
-        break;
+        return 'N';
       default:
-        value = 'U';
-        break;
+        return 'U';
     }
-    return value;
   }
-  
+
+  function getSessionId(session_text) {
+    return session_text.toString().trim().slice(0, session_text.indexOf(' ')).slice(1);
+  }
+
   function objectToArray(object) {
     return !Array.isArray(object) ? [object] : object;
   }
 
-  state.data.form.attendance_list.update_participant_cases.item = objectToArray(state.data.form.attendance_list.update_participant_cases.item);
-  
-  const session_text = dataValue('form.attendance_list.session')(state);
+  state.data.form.attendance_list.update_participant_cases.item = objectToArray(
+    state.data.form.attendance_list.update_participant_cases.item
+  );
 
-  const session_id = session_text.trim().slice(0, session_text.indexOf(' ')).slice(1);
+  const sessionText = dataValue('form.attendance_list.session')(state);
+  const sessionId = getSessionId(sessionText);
+  const sessionDate = dataValue("form.case['@date_modified']")(state);
 
-  state.data.dynamicFields = [
-    field(`Session_${session_id}__c`, getSessionValue()),
-    field(`Session_${session_id}_Date__c`, dataValue('form.attendance_list.date')(state)),
-  ];
+  state.data.form.attendance_list.update_participant_cases.item =
+    state.data.form.attendance_list.update_participant_cases.item.map(item => {
+      const sessionValue = getSessionValue(item.attendance_session);
+      return {
+        ...item,
+        dynamicFields: { [`Session_${sessionId}__c`]: sessionValue, [`Session_${sessionId}_Date__c`]: sessionDate },
+      };
+    });
 
   return state;
 });
+
 each(
-  merge(dataPath('form.attendance_list.update_participant_cases.item[*]'), fields(
-    field('intervention_name', dataValue('form.intervention_name'))
-    //field('case_id', dataValue('form.case.@case_id')),
-  )),
-  upsert( 'Attendance__c', 'CommCare_Ext_ID__c', state => ({
-    ...fields(...state.data.dynamicFields),
+  dataPath('form.attendance_list.update_participant_cases.item[*]'),
+  upsert('Attendance__c', 'CommCare_Ext_ID__c', state => ({
     ...fields(
-    relationship('Event__r', 'CommCare_Case_ID__c', dataValue('case_id')),
-    field('Name', dataValue('form.attendance_list.update_participant_cases.item.participant_id')),
-    field('CommCare_Ext_ID__c', state => {
-        var eventid = dataValue('intervention_name')(state);
-        var personid = state.data.case.index.parent['#text'];
+      // relationship('Event__r', 'CommCare_Case_ID__c', dataValue('case_id')),
+      field('Name', dataValue('participant_name')),
+      field('CommCare_Ext_ID__c', state => {
+        //   const eventid = dataValue('intervention_name')(state);
+        const eventid = dataValue('session_name')(state);
+        //   var personid = state.data.case.index.parent['#text'];
+        const personid = state.data['@id'];
         return personid + '-' + eventid;
       }),
-    relationship('Person_Attendance__r', 'Participant_Identification_Number_PID__c', dataValue('create_attendance_case.case.index.parent.#text')),
-  ),
- }))
+      relationship('Person_Attendance__r', 'Participant_Identification_Number_PID__c', state => state.data['@id'])
+    ),
+    ...state.data.dynamicFields,
+  }))
 );
