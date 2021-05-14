370042f38cff7828cba15b31cdce3d604abf211a
Berian <chaiwa.berian@gmail.com>
2021-05-14 10:29:10 +0200

    Update upsertAttendance.js

--- jobs/upsertAttendance.js
+++ jobs/upsertAttendance.js
@@ -56,7 +56,8 @@ each(
         const personid = state.data['@id'];
         return personid + '-' + eventid;
       }),
-      // @aleksa-krolls I
+      // @aleksa-krolls I get this error:
+     // INVALID_FIELD: Foreign key external ID: ce3f0b88-a612-4f5e-b26c-9888f65ee376 not found for field CommCare_Case_ID__c in entity Event__c
       relationship('Person_Attendance__r', 'Participant_Identification_Number_PID__c', state => state.data['@id'])
     ),
     ...state.data.dynamicFields,
