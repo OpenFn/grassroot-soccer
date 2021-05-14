9dd299a56b57559e758a5317d44d298fa537b452
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-14 10:02:31 -0400

    aissatou@openfn.org's change on OpenFn.org

--- jobs/upsertRegisterSkillsPlusClub.js
+++ jobs/upsertRegisterSkillsPlusClub.js
@@ -1,12 +1,51 @@
 // push to production
+alterState((state) => {
+  
+  function clean(str) {
+    if (!!str)
+      return str
+        .split('_')
+        .map(word => {
+          let new_word = word.toString().toLowerCase();
+          return new_word.slice(0, 1).toUpperCase() + new_word.slice(1);
+        })
+        .join(' ');
+  }
+  
+  const coachFieldNames = ['Coach_A__r','Coach_B__r','Coach_C__r','Coach_D__r']
+ 
+  state.data.destinationCoachFields = state.data.form.event_information.coaches
+  .split(' ')
+  .reduce(
+    (accumulator, currentValue, currentIndex) => [
+      ...accumulator,
+      (currentIndex < coachFieldNames.length && !!currentValue
+        ? relationship(coachFieldNames[currentIndex], 'CommCare_Ext_ID__c', currentValue)
+        : []),
+    ],
+    []
+  );
+
+  return state; 
+}); 
+
+
+
 upsert(
   'Event__c',
-  'CommCare_Ext_ID__c',
-  fields(
-    field('CommCare_Ext_ID__c', dataValue('form.name_of_skillz_plus_club')),
-    relationship('RecordType', 'Name', 'Intervention'),
-    relationship('Site__r', 'CommCare_Ext_ID__c', dataValue('form.skillz_plus_site')),
-    relationship('Venue__r', 'CommCare_Ext_ID__c', dataValue('form.skillz_plus_venue')),
-    relationship('Coach_A__r','CommCare_Ext_ID__c', dataValue('form.coaches'))
-  )
+  'CommCare_Case_ID__c',
+  state=>({
+  ...fields(
+      field('Name', dataValue('form.name_of_skillz_plus_club')),
+      field('CommCare_Ext_ID__c', dataValue('form.name_of_skillz_plus_club')),
+      feld('CommCare_Case_ID__c', dataValue('form.case.@case_id')),
+      relationship('RecordType', 'Name', 'Intervention'),
+      relationship('Site__r', 'CommCare_Ext_ID__c', dataValue('form.skillz_plus_site')),
+      relationship('Venue__r', 'CommCare_Ext_ID__c', dataValue('form.skillz_plus_venue')),
+      relationship('Coach_A__r','CommCare_Ext_ID__c', dataValue('form.coaches'))
+    ),
+    
+    ...fields(...state.data.destinationCoachFields)
+  
+  })
 );
