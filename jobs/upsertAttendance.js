3db20d75f27aa9f70baa86bb4bceda68dda7ffdc
Berian <chaiwa.berian@gmail.com>
2021-05-14 10:26:38 +0200

    Update upsertAttendance.js

--- jobs/upsertAttendance.js
+++ jobs/upsertAttendance.js
@@ -28,7 +28,7 @@ alterState(state => {
   const sessionText = dataValue('form.attendance_list.session')(state);
   const sessionId = getSessionId(sessionText);
 
-  //   @aleksa-krolls confirm the path for the session data
+  //   @aleksa-krolls confirm the path for the session date
   const sessionDate = dataValue("form.case['@date_modified']")(state);
 
   state.data.form.attendance_list.update_participant_cases.item =
