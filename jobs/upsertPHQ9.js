19dbbbd7bf4269f969ec2a1c87303a239f5d019e
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-17 12:18:44 -0400

    aissatou@openfn.org's change on OpenFn.org

--- jobs/upsertPHQ9.js
+++ jobs/upsertPHQ9.js
@@ -43,6 +43,12 @@ upsert(
     field('Curriculum__c', dataValue('form.hidden_properties.curriculum')),
     //field('Intervention__c', dataValue('form.hidden_properties.intervention')),
     field('Gender__c', dataValue('form.hidden_properties.gender')),
+    field('Participant_Name__c', state => {
+        const firstname = dataValue('form.hidden_properties.participant_first_name');
+        const lastname = dataValue('form.hidden_properties.participant_surname');
+        return firstname + ' ' + lastname;
+      }),
+    
     field('Participant_Name__c', dataValue('form.hidden_properties.participant_fullname')),
     field('Coach_Name__c', dataValue('form.hidden_properties.coach_name')),
     field('Venue__c', dataValue('form.hidden_properties.venue')),
