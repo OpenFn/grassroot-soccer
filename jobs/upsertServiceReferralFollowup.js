331a600f684a20f9a5f24c7b28e5b5d52ea67d3e
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-17 12:39:57 -0400

    aissatou@openfn.org's change on OpenFn.org

--- jobs/upsertServiceReferralFollowup.js
+++ jobs/upsertServiceReferralFollowup.js
@@ -50,7 +50,7 @@ upsert(
         )(state)
       )
     ),
-    field('HIV_STI_PREVENTION_Other_Service_Provider__c', state =>
+    field('HIV_STI_PREVENTION_Other_Service_Provid__c', state =>
       Boolean(
         dataValue(
           'form.referral_services.service_type.hiv_support__care.check_services_label.hiv_support_care_option6'
