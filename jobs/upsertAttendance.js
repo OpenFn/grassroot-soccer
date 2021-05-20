b2356750001dc5fb5a7af549f8929f14e4ca623c
Aleksa Krolls <aleksagk@gmail.com>
2021-05-20 18:28:44 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/upsertAttendance.js
+++ jobs/upsertAttendance.js
@@ -2,7 +2,7 @@ alterState(state => {
   return query(`SELECT Name FROM Event__c WHERE CommCare_Case_ID__c = 
   '${state.data.form.case['@case_id']}'`)(state)
         .then(() => {
-            console.log(state.data);
+            console.log(state.response.records[0]);
             return state;
         })
 }); 
