5f1901eaf71473089f8faed5e1f47d5c66a59c3c
Aleksa Krolls <aleksagk@gmail.com>
2021-05-20 16:44:26 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/2.c.upsertInterventionNotes.js
+++ jobs/2.c.upsertInterventionNotes.js
@@ -1,9 +1,10 @@
 //openfn.org source 
 upsert(
   'Event__c',
-  'Name',
+  'CommCare_Case_ID__c',
   fields(
-    field('Name', dataValue('form.intervention_name')),
+    field('CommCare_Case_ID__c', dataValue('form.case.@case_id')),
+    //field('Name', dataValue('form.intervention_name')),
     field('Notes__c', dataValue('form.intervention_notes_to_save')),
     relationship('RecordType', 'Name', 'Intervention')
   )
