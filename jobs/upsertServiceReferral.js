f8f5d566753b515d53b169ffd8434e0b888c9ec5
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-05-20 13:38:27 -0400

    aissatou@openfn.org's change on OpenFn.org

--- jobs/upsertServiceReferral.js
+++ jobs/upsertServiceReferral.js
@@ -149,9 +149,10 @@ upsert(
       'Sexual_and_GBV_Abuse_GRS_Staff__c',
       dataValue('form.referral_services.child_protection_support_services.abuse_reported_to')
     ),
+    
     field(
       'Legal_Other__c',
-      dataValue('form.referral_services.child_protection_support_services.type_of_post_violence_care')
+      dataValue('form.referral_services.legal_services.please_specify_other_legal')
     ),
     field(
       'Legal_Services_Other_GRS_Staff__c',
