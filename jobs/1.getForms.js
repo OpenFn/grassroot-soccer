3891fcacb5c3cd217c9416f4ae74411994e91225
Aleksa Krolls <aleksagk@gmail.com>
2021-06-03 07:27:00 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/1.getForms.js
+++ jobs/1.getForms.js
@@ -4,8 +4,8 @@ get(
     query: {
       limit: 1000, //max limit: 1000
       offset: state.meta && state.meta.next ? state.meta.limit + state.meta.offset : 0,
-      received_on_start: '2021-05-03',
-      received_on_end: '2021-05-15',
+      received_on_start: '2021-05-15',
+      received_on_end: '2021-05-27',
     },
   },
   state => {
