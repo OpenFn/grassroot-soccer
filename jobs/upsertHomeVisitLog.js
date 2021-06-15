3cdf2b7ee2b11600fac0b2e9cae65818c10b7268
mamadou <mamadou.lakhassane@gmail.com>
2021-06-15 12:33:46 +0000

    if no intervention_hidden query for eventId, #24

--- jobs/upsertHomeVisitLog.js
+++ jobs/upsertHomeVisitLog.js
@@ -1,13 +1,35 @@
 // push to production
-upsert(
-  'Home_Visit__c',
-  'CommCare_Ext_ID__c',
-  fields(
+alterState(state => {
+  const { form } = state.data;
+  const { basic_information } = form;
+  if (basic_information.intervention_hidden) {
+    state.data.eventField = relationship(
+      'Event__r',
+      'CommCare_Ext_ID__c',
+      dataValue('form.basic_information.intervention_hidden')
+    );
+    return state;
+  } else {
+    return query(
+      `SELECT Id, Event__c, CreatedDate, Person_Attendance__c
+    FROM Attendance__c
+    WHERE Person_Attendance__r.CommCare_External_ID__c = ${form.case['@case_id']}
+    ORDER BY CreatedDate DESC LIMIT 1`
+    )(state).then(state => {
+      const { records } = state.references[0];
+      const eventId = records[0].Event__c;
+      state.data.eventField = field('Event__c', eventId);
+    });
+  }
+});
+
+upsert('Home_Visit__c', 'CommCare_Ext_ID__c', state => ({
+  ...fields(
     field('CommCare_Ext_ID__c', dataValue('id')),
-    relationship('Event__r', 'CommCare_Ext_ID__c', dataValue('form.basic_information.intervention_hidden')),
+    // relationship('Event__r', 'CommCare_Ext_ID__c', dataValue('form.basic_information.intervention_hidden')),
     relationship('Person_visiting__r', 'Participant_Identification_Number_PID__c', dataValue('form.case.@case_id')),
     //=== NOTE: We do not need to map People information because Home Visit is related to Person. ======//
-    //This info already lives on the Person-level. 
+    //This info already lives on the Person-level.
     // field('First_Name__c', dataValue('form.basic_information.participant_first_name')),
     // field('Surname__c', dataValue('form.basic_information.participant_surname')),
     // field('Sex__c', dataValue('form.basic_information.gender')),
@@ -19,6 +41,7 @@ upsert(
     //field('From_Venue__c', dataValue('form.basic_information.venue_hidden')),
     field('Reason_for_Home_Visit__c', dataValue('form.participant_infomation.reason_for_home_visit')),
     field('Visit_Date__c', dataValue('form.participant_infomation.visit_information.visit_date')),
-    field('Additional_Comments__c', dataValue('form.administrative.visit_notes')),
-  )
-);
+    field('Additional_Comments__c', dataValue('form.administrative.visit_notes'))
+  ),
+  ...fields(...state.data.eventField),
+}));
