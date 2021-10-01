1c4e6dbbbf0fe96657313d6fabdc7ffc30266062
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-10-01 12:35:47 -0400

    elias@openfn.org's change on OpenFn.org

--- jobs/2.d.upsertRegisterParticipant.js
+++ jobs/2.d.upsertRegisterParticipant.js
@@ -74,7 +74,7 @@ each(
       field('CommCare_Ext_ID__c', state => {
         var eventid = `${state.data.intervention_name}` || `${state.data.eventName}`; //dataValue('intervention_name')(state) || `${state.data.eventName}`;
         var personid = state.data.case['@case_id'];
-        return personid + '-' + eventid.replace(/\//gi, '');
+        return removeEmojis(personid + '-' + eventid.replace(/\//gi, ''));
       }),
       relationship(
         //Attendance looks up to Persn via the case_id
