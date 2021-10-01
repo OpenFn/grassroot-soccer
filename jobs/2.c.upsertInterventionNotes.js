3cd7c0a8585a135850c1fe4455ddddae5b3c97db
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2021-10-01 13:11:09 -0400

    elias@openfn.org's change on OpenFn.org

--- jobs/2.c.upsertInterventionNotes.js
+++ jobs/2.c.upsertInterventionNotes.js
@@ -3,7 +3,7 @@ upsert(
   'Event__c',
   'CommCare_Case_ID__c',
   fields(
-    field('CommCare_Case_ID__c', removeEmojis(dataValue('form.case.@case_id'))),
+    field('CommCare_Case_ID__c', dataValue('form.case.@case_id')),
     //field('Name', dataValue('form.intervention_name')),
     field('Notes__c', dataValue('form.intervention_notes_to_save')),
     relationship('RecordType', 'Name', 'Intervention')
