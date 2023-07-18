4c6ec8f2feb7f3a18554dc7335fc76a4778a872b
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2023-07-18 05:15:29 -0500

    aissatou@openfn.org's change on OpenFn.org

--- jobs/upsertPostChallenges.js
+++ jobs/upsertPostChallenges.js
@@ -31,13 +31,13 @@ fn(state => {
   fn(state => {
     const { form } = state.data;
   
-    const formVersion = Object.keys(form).find(key => key.startsWith('portuguese_pre_challenge'));
+    const formVersion = Object.keys(form).find(key => key.startsWith('portuguese_post_challenge'));
   
     console.log("form version", formVersion);
   
     const sfFieldMapping = {
-      'portuguese_pre_challenge_gcr_-_skillz_malaria': {
-        Post_1__c: 'form.portuguese_pre_challenge_gcr_-_skillz_malaria.dormir_dentro_da_rede_mosquiteira_tratada_todas_as_noites_pode_proteger_me_',
+      'portuguese_post_challenge_gcr_-_skillz_malaria': {
+        Post_1__c: 'form.portuguese_post_challenge_gcr_-_skillz_malaria.dormir_dentro_da_rede_mosquiteira_tratada_todas_as_noites_pode_proteger_me_',
         Post_2__c: 'form.portuguese_post_challenge_gcr_-_skillz_malaria.posso_esperar_uns_dias_antes_de_fazer_o_teste_de_malria_se_tiver_febres',
         Post_3__c: 'form.portuguese_post_challenge_gcr_-_skillz_malaria.um_mdico_tradicionalcurandeiro_pode_me_dar_um_tratamento_para_curar_a_malri',
         Post_4__c: 'form.portuguese_post_challenge_gcr_-_skillz_malaria.se_tiver_malria_e_no_for_tratado_posso_transmitir_a_malria_a_algum',
@@ -173,7 +173,6 @@ fn(state => {
   });
   
   fn(state => {
-    console.log("test");
     console.log(state.pluckSfValue('Post_1__c'));
     return state;
   });
