59d1a95a0254c6d35b99f72d8f9c6eb568cb3d5c
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2023-07-18 06:21:41 -0500

    aissatou@openfn.org's change on OpenFn.org

--- jobs/upsertPreChallenges.js
+++ jobs/upsertPreChallenges.js
@@ -35,7 +35,7 @@ fn(state => {
 fn(state => {
   const { form } = state.data;
 
-  const formVersion = Object.keys(form).find(key => key.startsWith('pre_challenge'));
+  const formVersion = Object.keys(form).find(key => key.contains('pre_challenge'));
 
   console.log("form version", formVersion);
 
