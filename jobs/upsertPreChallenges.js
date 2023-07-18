3643e197e896e40b3155c00225aeba33502ae2c9
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2023-07-18 06:26:47 -0500

    aissatou@openfn.org's change on OpenFn.org

--- jobs/upsertPreChallenges.js
+++ jobs/upsertPreChallenges.js
@@ -232,7 +232,7 @@ fn(state => {
       }),
       field('Date_Pre_Administered__c', dataValue('form.date')),
       field('Pre_1__c', state => {
-        const value = dataValue(state.pluckSfValue('Pre_1__c'));
+        const value = dataValue(state.pluckSfValue('Pre_1__c'))(state);
         console.log("value", value);
         console.log("transformed", state.helperFunctions.transform(value))
         return state.helperFunctions.transform(value);
