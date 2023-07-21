d1b282d85f93a71ba840686e2cf692850c284253
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2023-07-21 05:27:21 -0500

    aissatou@openfn.org's change on OpenFn.org

--- jobs/upsertCoachSessionRegister.js
+++ jobs/upsertCoachSessionRegister.js
@@ -68,7 +68,7 @@ fn(state => {
     upsert('Attendance__c', 'CommCare_Ext_ID__c', state => ({
       ...fields(
         //relationship('RecordType', 'Name', 'Intervention (Staff)'),
-        field('RecordType', '012500000009jq0AAA'),
+        field('RecordTypeId', '012500000009jq0AAA'),
         relationship('Event__r', 'CommCare_Case_ID__c', caseid),
         relationship('Person_Attendance__r', 'CommCare_Ext_ID__c', state => {
           const coach_name = state.data;
