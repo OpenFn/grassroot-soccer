4b293113d4a93b8688f149a65a5928118180ed24
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2023-09-07 17:26:59 -0500

    aissatou@openfn.org's change on OpenFn.org

--- jobs/upsertPreChallenges.js
+++ jobs/upsertPreChallenges.js
@@ -313,7 +313,7 @@ fn(state => {
     'Attendance__c',
     'CommCare_Ext_ID__c',
     fields(
-      field('Event__c', dataValue('form.hidden_properties.intervention_name')),
+      field('Event__c',  replaceAccents(dataValue('form.hidden_properties.intervention_name')(state))),
       //relationship('Event__r', 'CommCare_Ext_ID__c', dataValue('form.hidden_properties.intervention_name')),
       relationship('Person_Attendance__r', 'Participant_Identification_Number_PID__c', dataValue('form.case.@case_id')),
       // relationship('Person_Attendance__r','CommCare_Ext_ID__c', state => {
