b22cb0542d9609b989e528accd0ecce1e9105d91
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2024-08-06 09:46:31 -0400

    aissatou@openfn.org's change on OpenFn.org

--- jobs/1.getForms.js
+++ jobs/1.getForms.js
@@ -6,7 +6,7 @@ get(
       offset: state.meta && state.meta.next ? state.meta.limit + state.meta.offset : 0,
       received_on_start: '2024-05-16',
       received_on_end: '2024-05-30',
-      xmlns: 'http://openrosa.org/formdesigner/D771417E-354E-4906-A686-DF0BA230F16A',
+      xmlns: 'http://openrosa.org/formdesigner/6F130211-5A05-4915-8F55-A413A6B9D61B',
     },
   },
   state => {
