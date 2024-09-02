f177afd65e52f733d8090ff1548e5402e2cc9ad9
Rita Zágoni <zagoni.rita@gmail.com>
2024-09-02 12:41:28 +0200

    rita@openfn.org's change on OpenFn.org

--- jobs/upsertPostChallenges.js
+++ jobs/upsertPostChallenges.js
@@ -534,7 +534,47 @@ fn(state => {
           if (state.pluckSfValue('Post_20__c') == null) return undefined
           const value = dataValue(state.pluckSfValue('Post_20__c'))(state); 
           return state.helperFunctions.transform(value);
-        })
+        }),
+      field('Pre_21__c', state => {
+        if (state.pluckSfValue('Pre_21__c') == null) return undefined
+        const value = dataValue(state.pluckSfValue('Pre_21__c'))(state); 
+        return state.helperFunctions.transform(value);
+      }),
+      field('Pre_22__c', state => {
+        if (state.pluckSfValue('Pre_22__c') == null) return undefined
+        const value = dataValue(state.pluckSfValue('Pre_22__c'))(state); 
+        return state.helperFunctions.transform(value);
+      }),
+      field('Pre_23__c', state => {
+        if (state.pluckSfValue('Pre_23__c') == null) return undefined
+        const value = dataValue(state.pluckSfValue('Pre_23__c'))(state); 
+        return state.helperFunctions.transform(value);
+      }),
+      field('Pre_24__c', state => {
+        if (state.pluckSfValue('Pre_24__c') == null) return undefined
+        const value = dataValue(state.pluckSfValue('Pre_24__c'))(state); 
+        return state.helperFunctions.transform(value);
+      }),
+      field('Pre_25__c', state => {
+        if (state.pluckSfValue('Pre_25__c') == null) return undefined
+        const value = dataValue(state.pluckSfValue('Pre_25__c'))(state); 
+        return state.helperFunctions.transform(value);
+      }),
+      field('Pre_26__c', state => {
+        if (state.pluckSfValue('Pre_26__c') == null) return undefined
+        const value = dataValue(state.pluckSfValue('Pre_26__c'))(state); 
+        return state.helperFunctions.transform(value);
+      }),
+      field('Pre_27__c', state => {
+        if (state.pluckSfValue('Pre_27__c') == null) return undefined
+        const value = dataValue(state.pluckSfValue('Pre_27__c'))(state); 
+        return state.helperFunctions.transform(value);
+      }),
+      field('Pre_28__c', state => {
+        if (state.pluckSfValue('Pre_28__c') == null) return undefined
+        const value = dataValue(state.pluckSfValue('Pre_28__c'))(state); 
+        return state.helperFunctions.transform(value);
+      })
       )
     )(state);
   });
